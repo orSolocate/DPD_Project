3f8613e40317665234b8514bd9356fc8b332dbfa
Or <or.ba402@gmail.com>
2019-12-19 21:54:10 +0200

    ILD Scheme for RFWebLab

---
+++ ILC_Scheme_RFWebLab.m
@@ -0,0 +1,62 @@
+function [x_opt, error_vec_plot] = ILC_Scheme_RFWebLab(x,y,orders)
+
+% Error requirement
+
+err_req = 1e-9;
+error_vec_plot = [];
+ii = 0;
+
+% Block to get PA model
+
+
+weblab_delay           = finddelay(x, y);
+if(weblab_delay >=0)
+    avg_gain_weblab    = abs(mean(y(weblab_delay+1:end)./x(1:end-weblab_delay)));
+    PD_coef_Matrix_WL  = Get_coef_MP(y(weblab_delay+1:end)', x(1:end-weblab_delay)', orders(1), orders(2));
+end
+if(weblab_delay < 0)
+    avg_gain_weblab    = abs(mean(y(1:end-weblab_delay)./x(weblab_delay+1:end)));
+    PD_coef_Matrix_WL  = Get_coef_MP(y(1:end-weblab_delay)', x(weblab_delay+1:end)', orders(1), orders(2));
+end
+y_d_WL                 = x.*avg_gain_weblab;
+x_opt                  = zeros(length(y_d_WL),1);
+
+%numDataPts = length(y_d_WL);
+%halfDataPts = round(numDataPts/2); % Half is relevant for simulation purposes only!
+
+%coefMat = Get_Coefficients_Matrix(inDataPA(1:halfDataPts), outDataPA(1:halfDataPts), memLen, degLen);
+
+% error calculation
+n = orders(2)+1;
+while (n<length(y_d_WL))                                         %run the system as live
+    x_opt(n) = PD_MP(y_d_WL(n-orders(2):n), PD_coef_Matrix_WL, orders(1),orders(2));
+    n                  = n+1;
+end
+
+u=x./avg_gain_weblab;
+
+[y, RMSout, Idc, Vdc]  = RFWebLab_PA_meas_v1_1(u);
+
+error_vec = y_d_WL(orders(1):length(y_d_WL)) - y(orders(1):length(y_d_WL));
+error = norm(error_vec, 2);
+error_vec_plot = [error_vec_plot error];
+
+gamma = 2/(2*avg_gain_weblab);
+
+while (error > err_req)
+    ii = ii + 1;
+    if (ii == 3)
+        break;
+    end
+    % "iteration k"
+    u(orders(1):length(y_d_WL)) = u(orders(1):length(y_d_WL)) + gamma * error_vec;
+    % error calculation
+    max(abs(u))
+    [y, RMSout, Idc, Vdc]  = RFWebLab_PA_meas_v1_1(u);
+    pspectrum(y)
+    error_vec = y_d_WL(orders(1):length(y_d_WL)) - y(orders(1):length(y_d_WL));
+    error = norm(error_vec, 2);
+    error_vec_plot = [error_vec_plot error];
+end
+
+end
---
+++ main_DPD_ILC_RFWebLab.m
@@ -0,0 +1,68 @@
+clc; clear; close all;                                     %start freash
+
+%% General User inputs
+signal            = '100MHzLTE.mat';
+model_run_period  = 100000;                                %num of samples to run in the model
+start_pos_sig     = 250001;
+end_pos_sig       = start_pos_sig+model_run_period-1;
+coef_update_time  = 10000000;
+phase_ud_time     = 10000000;
+phase_diff        = 0.1;
+
+
+%% RF-WEBLAB User inputs
+memory_deg_weblab   = 3;                                   %K in the MP model
+memory_depth_weblab = 5;                                   %M in the MP model
+Ma_WL = 9; Mb_WL = 2; Mc_WL = 0;                           %memory deg for GMP model
+Ka_WL = 8; Kb_WL = 5; Kc_WL = 0;                           %non-linearity deg for GMP
+P_WL = 5; Q_WL = 0;                                        %cross-terms for GMP
+
+
+%% RF-WEBLAB Initialize
+load(signal);                                              %load signal
+x                = waveform(start_pos_sig:end_pos_sig);
+[y, RMSout, Idc, Vdc]  = RFWebLab_PA_meas_v1_1(x);
+
+%% ILC_Scheme
+orders=[memory_deg_weblab,memory_depth_weblab];
+[x_opt, error_vec_plot] = ILC_Scheme_RFWebLab(x,y, orders);
+figure; 
+plot(1:length(error_vec_plot), error_vec_plot);
+xlabel('Iterations');
+ylabel('Error');
+title('Error vs. Iteration Number');
+grid;
+set(findall(gca, 'Type', 'Line'),'LineWidth',2);
+set(findall(gcf,'-property','FontSize'),'FontSize',18);
+set(findall(gcf,'-property','GridAlpha'),'GridAlpha',1);
+
+
+
+
+%% RF-WEBLAB Ploting
+[y_RFW_MP, RMSout, Idc, Vdc]  = RFWebLab_PA_meas_v1_1(x_opt_weblab_MP);
+[y_RFW_GMP, RMSout, Idc, Vdc] = RFWebLab_PA_meas_v1_1(x_opt_weblab_GMP);
+transfer_no_PD_weblab         = y./x;
+transfer_with_MP_PD_weblab    = y_RFW_MP./x_opt_weblab_MP;
+transfer_with_GMP_PD_weblab   = y_RFW_GMP./x_opt_weblab_GMP;
+
+figure
+pspectrum(y_d_WL(memory_depth_weblab+1:end))
+hold on
+pspectrum(y)
+hold on
+pspectrum(y_RFW_MP(memory_depth_weblab+1:end))
+hold on
+pspectrum(y_RFW_GMP)
+legend('y_d','no PD','with MP PD','with GMP PD')
+
+figure;
+plot(abs(y_d_WL./avg_gain_weblab), 20*log10(abs(transfer_no_PD_weblab)), 'o');
+hold on;
+plot(abs(y_d_WL./avg_gain_weblab), 20*log10(abs(transfer_with_MP_PD_weblab)), '+');
+hold on;
+plot(abs(y_d_WL./avg_gain_weblab), 20*log10(abs(transfer_with_GMP_PD_weblab)), '*');
+legend('Without DPD','with MP PD','with GMP PD')
+xlabel('abs(input)');
+ylabel('Magnitude');
+title('Linearity Comparison');
