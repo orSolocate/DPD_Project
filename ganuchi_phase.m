223c2f9e939edee475732e55a53c199d17281a91
Or <or.ba402@gmail.com>
2020-01-18 00:10:07 +0200

    Ganuchi update: no need for finddelay fix

--- ganuchi_phase.m
+++ ganuchi_phase.m
@@ -2,24 +2,26 @@ clc; clear all; close all
 %% Comment are for matlab amplifier
 %amp_data          = 'simrfV2_powamp_dpd_data.mat';
 %load(amp_data);                                            %load amp data
-signal            = '80MHz_at_200MHz';
+signal            = 'y_WL_100MHzLTE.mat';
 load(signal);                                              %load signal
-x                     = x(1:5000)./(2*norm(x,2));
-RMS_in                = 10*log10( norm(x)^2/50/length(x)) + 30 + 20;
-[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x, RMS_in);
+x=waveform(start_pos_sig:start_pos_sig+X_length-1); %if you load the file:
+%y_WL_100MHzLTE.mat
+%x                     = x(1:5000)./(2*norm(x,2));
+%RMS_in                = 10*log10( norm(x)^2/50/length(x)) + 30 + 20;
+%[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x, RMS_in);
 %mem_depth = 4 ;                                        
 %mem_deg   = 5 ;  
 %AMP_coef_Matrix  = Get_coef_MP(inDataPA, outDataPA, mem_deg, mem_depth);
 %y     = [zeros(mem_depth,1); Get_model_output_MP(AMP_coef_Matrix, x, mem_deg, mem_depth)];
 %%
-WL_delay = finddelay(x, y);
-if(WL_delay >= 0)
-    y = y(WL_delay+1:end);
-    x = x(1:end-WL_delay);
-elseif(WL_delay < 0)
-    y = y(1:end+WL_delay);
-    x = x(1-WL_delay:end);
-end
+% WL_delay = finddelay(x, y);
+% if(WL_delay >= 0)
+%     y = y(WL_delay+1:end);
+%     x = x(1:end-WL_delay);
+% elseif(WL_delay < 0)
+%     y = y(1:end+WL_delay);
+%     x = x(1-WL_delay:end);
+% end
 %%
 N = length(x);
 % window preparation
@@ -29,7 +31,7 @@ X = fft(x.*win);
 % fft of the second signal
 Y = fft(y.*win);
 
-fs=SR
+%fs=SR
 
 Fv = linspace(0, 1, fix(length(x)/2)+1)*fs/2;           % Frequency Vector
 Iv = 1:length(Fv);
@@ -60,3 +62,8 @@ legend('phi(f/fs)','linear fit');
 
 
 t0=-1*real(slope)/(2*pi)
+
+%T=1/fs 
+%t0=t0_normalized/fs
+
+Samples_delay=round(t0)
