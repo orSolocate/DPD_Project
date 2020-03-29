4c25d3d920053530e3f539e544664a0057361e7a
Or <or.ba402@gmail.com>
2020-03-29 22:23:44 +0300

    Not significant changes I made locally. nothing else to commit and upload on my side.
    Or

--- MP GMP/functions/general functions/ganuchi_phase.m
+++ MP GMP/functions/general functions/ganuchi_phase.m
@@ -18,8 +18,8 @@ load(signal);
 x                     = waveform(1:50000)./(2*norm(waveform,2));
 %x                     = x(1:5000)./(2*norm(x,2));
 RMS_in                = 10*log10( norm(x)^2/50/length(x)) + 30 + 20;
-%[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x, RMS_in);
-[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x);
+[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x, RMS_in);
+%[y, ~, ~, ~] = RFWebLab_PA_meas_v1_1(x);
 
 %%
 N = length(x);
@@ -35,10 +35,10 @@ fs=1228800;
 Fv = linspace(0, 1, fix(length(x)/2)+1)*fs;           % Frequency Vector
 Iv=1:length(Fv);
 
-phs_x = unwrap(angle(X));
-phs_y = unwrap(angle(Y));
-% phs_x = (angle(X));
-% phs_y = (angle(Y));
+%phs_x = unwrap(angle(X));
+%phs_y = unwrap(angle(Y));
+phs_x = (angle(X));
+phs_y = (angle(Y));
 
 phsv_x = phs_x(Iv); 
 phsv_y = phs_y(Iv); 
@@ -66,7 +66,7 @@ plot(freq, yFit, 'b', 'LineWidth', 2);
 legend('phi(f/fs)','linear fit');
 
 
-t0=-1*real(slope)/(2*pi);
+t0=-1*real(slope)/(2*pi)
 
 %T=1/fs 
 %t0=t0_normalized/fs
